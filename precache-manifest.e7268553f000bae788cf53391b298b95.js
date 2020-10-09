01382c8dc0e7524c7997d8b8a6b632fbc016b7e0
st-minju <kimminju0115@gmail.com>
2020-10-10 08:26:24 +0900

    Updates

---
+++ precache-manifest.e7268553f000bae788cf53391b298b95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b91e866ce956503b8d87162cfdf20816",
+    "url": "/index.html"
+  },
+  {
+    "revision": "565cf0c27c311a739713",
+    "url": "/static/css/main.404f6934.chunk.css"
+  },
+  {
+    "revision": "11748d6dc8da0fe94e79",
+    "url": "/static/js/2.8f3b569d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8f3b569d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "565cf0c27c311a739713",
+    "url": "/static/js/main.281d4fa7.chunk.js"
+  },
+  {
+    "revision": "bd7f4f55b55e38cea2f1",
+    "url": "/static/js/runtime-main.58002af7.js"
+  }
+]);
